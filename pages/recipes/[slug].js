ebe6801ccdd6686a2cc35905f65ab7a9db6ab596
Sebastian Asis <sasisromero10@gmail.com>
2021-04-15 00:02:57 -0500

    :dart: IMPROVE: incremental statisc props

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,4 +1,7 @@
 import { createClient } from 'contentful'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import Image from 'next/image'
+import Skeleton from '../../components/Skeleton'
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -18,7 +21,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false
+    fallback: true
   }
 }
 
@@ -29,17 +32,70 @@ export const getStaticProps = async ({ params }) => {
   })
 
   return {
-    props: { recipe: items[0] }
+    props: { recipe: items[0] },
+    revalidate: 1
   }
 
 }
 
 export default function RecipeDetails({ recipe }) {
-  console.log(recipe)
-  
+  if (!recipe) return <Skeleton />
+  const { featuredImage, title, cookingTime, ingredients, method } = recipe.fields
+
   return (
     <div>
-      Recipe Details
+      <div className="banner">
+        <Image 
+          src={'https:' + featuredImage.fields.file.url}
+          width={featuredImage.fields.file.details.image.width}
+          height={featuredImage.fields.file.details.image.height}
+        />
+        <h2>{ title }</h2>
+      </div>
+
+      <div className="info">
+        <p>Takes about { cookingTime } mins to cook.</p>
+        <h3>Ingredients:</h3>
+
+        {ingredients.map(ing => (
+          <span key={ing}>{ ing }</span>
+        ))}
+      </div>
+        
+      <div className="method">
+        <h3>Method:</h3>
+        <div >{documentToReactComponents(method)}</div>
+      </div>
+
+      <style jsx>{`
+        h2,h3 {
+          text-transform: uppercase;
+        }
+        .banner h2 {
+          margin: 0;
+          background: #fff;
+          display: inline-block;
+          padding: 20px;
+          position: relative;
+          top: -60px;
+          left: -10px;
+          transform: rotateZ(-1deg);
+          box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
+        }
+        .info p {
+          margin: 0;
+        }
+        .info span::after {
+          content: ", ";
+        }
+        .info span:last-child::after {
+          content: ".";
+        }
+
+        .mcontent{
+          max-width: 1300px;
+        }
+      `}</style>
     </div>
   )
 }
